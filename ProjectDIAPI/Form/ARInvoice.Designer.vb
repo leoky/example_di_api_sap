a72e8056e31c3edb8e3c48551d9b8817d7959af6
Leonardy Khanady <leonardy189@gmail.com>
2019-06-26 10:37:21 +0700

    invoice

---
+++ ProjectDIAPI/Form/ARInvoice.Designer.vb
@@ -0,0 +1,422 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class ARInvoice
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ItemCode = New System.Windows.Forms.DataGridViewButtonColumn()
+        Me.Quantity = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.ItemName = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.UnitPrice = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.DataGridView = New System.Windows.Forms.DataGridView()
+        Me.Total = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.btnNext = New System.Windows.Forms.Button()
+        Me.btnFind = New System.Windows.Forms.Button()
+        Me.btnUpdate = New System.Windows.Forms.Button()
+        Me.ComboBoxCopyTo = New System.Windows.Forms.ComboBox()
+        Me.TextBoxDocStatus = New System.Windows.Forms.TextBox()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.TextBoxDocTotal = New System.Windows.Forms.TextBox()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.TextBoxDocNumber = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.btnPrevious = New System.Windows.Forms.Button()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.btnCancel = New System.Windows.Forms.Button()
+        Me.BtnOK = New System.Windows.Forms.Button()
+        Me.DateTimePickerTaxDate = New System.Windows.Forms.DateTimePicker()
+        Me.DateTimePickerDocDueDate = New System.Windows.Forms.DateTimePicker()
+        Me.DateTimePickerDocDate = New System.Windows.Forms.DateTimePicker()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.TextBoxBPName = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TextBoxBPCode = New System.Windows.Forms.TextBox()
+        Me.btnChooseCustomer = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        CType(Me.DataGridView, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'ItemCode
+        '
+        Me.ItemCode.HeaderText = "Item No"
+        Me.ItemCode.Name = "ItemCode"
+        '
+        'Quantity
+        '
+        Me.Quantity.HeaderText = "Quantity"
+        Me.Quantity.Name = "Quantity"
+        Me.Quantity.Resizable = System.Windows.Forms.DataGridViewTriState.[True]
+        Me.Quantity.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(678, 636)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(61, 17)
+        Me.Label10.TabIndex = 129
+        Me.Label10.Text = "Copy To"
+        '
+        'ItemName
+        '
+        Me.ItemName.HeaderText = "Item Description"
+        Me.ItemName.MinimumWidth = 200
+        Me.ItemName.Name = "ItemName"
+        Me.ItemName.Width = 200
+        '
+        'UnitPrice
+        '
+        Me.UnitPrice.HeaderText = "Unit Price"
+        Me.UnitPrice.Name = "UnitPrice"
+        Me.UnitPrice.Resizable = System.Windows.Forms.DataGridViewTriState.[True]
+        Me.UnitPrice.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable
+        '
+        'DataGridView
+        '
+        Me.DataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.ItemCode, Me.ItemName, Me.Quantity, Me.UnitPrice, Me.Total})
+        Me.DataGridView.Location = New System.Drawing.Point(29, 263)
+        Me.DataGridView.Name = "DataGridView"
+        Me.DataGridView.RowTemplate.Height = 24
+        Me.DataGridView.Size = New System.Drawing.Size(912, 246)
+        Me.DataGridView.TabIndex = 121
+        '
+        'Total
+        '
+        Me.Total.HeaderText = "Total"
+        Me.Total.Name = "Total"
+        Me.Total.ReadOnly = True
+        Me.Total.Resizable = System.Windows.Forms.DataGridViewTriState.[True]
+        Me.Total.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable
+        '
+        'btnNext
+        '
+        Me.btnNext.Location = New System.Drawing.Point(627, 42)
+        Me.btnNext.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.btnNext.Name = "btnNext"
+        Me.btnNext.Size = New System.Drawing.Size(35, 25)
+        Me.btnNext.TabIndex = 120
+        Me.btnNext.Text = "<"
+        Me.btnNext.UseVisualStyleBackColor = True
+        '
+        'btnFind
+        '
+        Me.btnFind.Location = New System.Drawing.Point(69, 90)
+        Me.btnFind.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.btnFind.Name = "btnFind"
+        Me.btnFind.Size = New System.Drawing.Size(75, 36)
+        Me.btnFind.TabIndex = 116
+        Me.btnFind.Text = "Find"
+        Me.btnFind.UseVisualStyleBackColor = True
+        '
+        'btnUpdate
+        '
+        Me.btnUpdate.Location = New System.Drawing.Point(232, 90)
+        Me.btnUpdate.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.btnUpdate.Name = "btnUpdate"
+        Me.btnUpdate.Size = New System.Drawing.Size(75, 36)
+        Me.btnUpdate.TabIndex = 118
+        Me.btnUpdate.Text = "Update"
+        Me.btnUpdate.UseVisualStyleBackColor = True
+        '
+        'ComboBoxCopyTo
+        '
+        Me.ComboBoxCopyTo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.ComboBoxCopyTo.Enabled = False
+        Me.ComboBoxCopyTo.FormattingEnabled = True
+        Me.ComboBoxCopyTo.Items.AddRange(New Object() {"A / R Invoice"})
+        Me.ComboBoxCopyTo.Location = New System.Drawing.Point(757, 636)
+        Me.ComboBoxCopyTo.Name = "ComboBoxCopyTo"
+        Me.ComboBoxCopyTo.Size = New System.Drawing.Size(184, 24)
+        Me.ComboBoxCopyTo.TabIndex = 128
+        '
+        'TextBoxDocStatus
+        '
+        Me.TextBoxDocStatus.Enabled = False
+        Me.TextBoxDocStatus.Location = New System.Drawing.Point(474, 175)
+        Me.TextBoxDocStatus.Name = "TextBoxDocStatus"
+        Me.TextBoxDocStatus.Size = New System.Drawing.Size(265, 22)
+        Me.TextBoxDocStatus.TabIndex = 127
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(356, 175)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(77, 17)
+        Me.Label9.TabIndex = 126
+        Me.Label9.Text = "Doc Status"
+        '
+        'TextBoxDocTotal
+        '
+        Me.TextBoxDocTotal.Location = New System.Drawing.Point(627, 532)
+        Me.TextBoxDocTotal.Name = "TextBoxDocTotal"
+        Me.TextBoxDocTotal.Size = New System.Drawing.Size(314, 22)
+        Me.TextBoxDocTotal.TabIndex = 125
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(560, 532)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(40, 17)
+        Me.Label8.TabIndex = 124
+        Me.Label8.Text = "Total"
+        '
+        'TextBoxDocNumber
+        '
+        Me.TextBoxDocNumber.Enabled = False
+        Me.TextBoxDocNumber.Location = New System.Drawing.Point(150, 152)
+        Me.TextBoxDocNumber.Name = "TextBoxDocNumber"
+        Me.TextBoxDocNumber.Size = New System.Drawing.Size(130, 22)
+        Me.TextBoxDocNumber.TabIndex = 123
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(49, 155)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(87, 17)
+        Me.Label7.TabIndex = 122
+        Me.Label7.Text = "Doc Number"
+        '
+        'btnPrevious
+        '
+        Me.btnPrevious.Location = New System.Drawing.Point(668, 42)
+        Me.btnPrevious.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.btnPrevious.Name = "btnPrevious"
+        Me.btnPrevious.Size = New System.Drawing.Size(33, 25)
+        Me.btnPrevious.TabIndex = 119
+        Me.btnPrevious.Text = ">"
+        Me.btnPrevious.UseVisualStyleBackColor = True
+        '
+        'btnAdd
+        '
+        Me.btnAdd.Location = New System.Drawing.Point(150, 90)
+        Me.btnAdd.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(75, 36)
+        Me.btnAdd.TabIndex = 117
+        Me.btnAdd.Text = "Add"
+        Me.btnAdd.UseVisualStyleBackColor = True
+        '
+        'btnCancel
+        '
+        Me.btnCancel.Location = New System.Drawing.Point(128, 636)
+        Me.btnCancel.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.btnCancel.Name = "btnCancel"
+        Me.btnCancel.Size = New System.Drawing.Size(93, 32)
+        Me.btnCancel.TabIndex = 115
+        Me.btnCancel.Text = "Cancel"
+        Me.btnCancel.UseVisualStyleBackColor = True
+        '
+        'BtnOK
+        '
+        Me.BtnOK.Location = New System.Drawing.Point(29, 636)
+        Me.BtnOK.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.BtnOK.Name = "BtnOK"
+        Me.BtnOK.Size = New System.Drawing.Size(93, 32)
+        Me.BtnOK.TabIndex = 114
+        Me.BtnOK.Text = "Add"
+        Me.BtnOK.UseVisualStyleBackColor = True
+        '
+        'DateTimePickerTaxDate
+        '
+        Me.DateTimePickerTaxDate.Format = System.Windows.Forms.DateTimePickerFormat.Custom
+        Me.DateTimePickerTaxDate.Location = New System.Drawing.Point(474, 136)
+        Me.DateTimePickerTaxDate.Margin = New System.Windows.Forms.Padding(4)
+        Me.DateTimePickerTaxDate.Name = "DateTimePickerTaxDate"
+        Me.DateTimePickerTaxDate.Size = New System.Drawing.Size(265, 22)
+        Me.DateTimePickerTaxDate.TabIndex = 113
+        Me.DateTimePickerTaxDate.Value = New Date(2019, 6, 22, 23, 37, 16, 0)
+        '
+        'DateTimePickerDocDueDate
+        '
+        Me.DateTimePickerDocDueDate.Format = System.Windows.Forms.DateTimePickerFormat.Custom
+        Me.DateTimePickerDocDueDate.Location = New System.Drawing.Point(474, 104)
+        Me.DateTimePickerDocDueDate.Margin = New System.Windows.Forms.Padding(4)
+        Me.DateTimePickerDocDueDate.Name = "DateTimePickerDocDueDate"
+        Me.DateTimePickerDocDueDate.Size = New System.Drawing.Size(265, 22)
+        Me.DateTimePickerDocDueDate.TabIndex = 112
+        Me.DateTimePickerDocDueDate.Value = New Date(2019, 6, 22, 23, 36, 58, 0)
+        '
+        'DateTimePickerDocDate
+        '
+        Me.DateTimePickerDocDate.Format = System.Windows.Forms.DateTimePickerFormat.Custom
+        Me.DateTimePickerDocDate.Location = New System.Drawing.Point(474, 73)
+        Me.DateTimePickerDocDate.Margin = New System.Windows.Forms.Padding(4)
+        Me.DateTimePickerDocDate.Name = "DateTimePickerDocDate"
+        Me.DateTimePickerDocDate.Size = New System.Drawing.Size(265, 22)
+        Me.DateTimePickerDocDate.TabIndex = 111
+        Me.DateTimePickerDocDate.Value = New Date(2019, 6, 22, 23, 37, 9, 0)
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(357, 110)
+        Me.Label6.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(93, 17)
+        Me.Label6.TabIndex = 110
+        Me.Label6.Text = "Delivery Date"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(356, 139)
+        Me.Label5.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(106, 17)
+        Me.Label5.TabIndex = 109
+        Me.Label5.Text = "Document Date"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(357, 79)
+        Me.Label4.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(89, 17)
+        Me.Label4.TabIndex = 108
+        Me.Label4.Text = "Posting Date"
+        '
+        'TextBoxBPName
+        '
+        Me.TextBoxBPName.Location = New System.Drawing.Point(148, 221)
+        Me.TextBoxBPName.Margin = New System.Windows.Forms.Padding(4)
+        Me.TextBoxBPName.Name = "TextBoxBPName"
+        Me.TextBoxBPName.Size = New System.Drawing.Size(132, 22)
+        Me.TextBoxBPName.TabIndex = 107
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(69, 224)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(45, 17)
+        Me.Label3.TabIndex = 106
+        Me.Label3.Text = "Name"
+        '
+        'TextBoxBPCode
+        '
+        Me.TextBoxBPCode.Location = New System.Drawing.Point(148, 190)
+        Me.TextBoxBPCode.Margin = New System.Windows.Forms.Padding(4)
+        Me.TextBoxBPCode.Name = "TextBoxBPCode"
+        Me.TextBoxBPCode.Size = New System.Drawing.Size(132, 22)
+        Me.TextBoxBPCode.TabIndex = 105
+        '
+        'btnChooseCustomer
+        '
+        Me.btnChooseCustomer.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnChooseCustomer.ForeColor = System.Drawing.SystemColors.ActiveBorder
+        Me.btnChooseCustomer.Location = New System.Drawing.Point(285, 192)
+        Me.btnChooseCustomer.Margin = New System.Windows.Forms.Padding(4)
+        Me.btnChooseCustomer.Name = "btnChooseCustomer"
+        Me.btnChooseCustomer.Size = New System.Drawing.Size(21, 20)
+        Me.btnChooseCustomer.TabIndex = 104
+        Me.btnChooseCustomer.TabStop = False
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(68, 192)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(68, 17)
+        Me.Label2.TabIndex = 103
+        Me.Label2.Text = "Customer"
+        '
+        'ARInvoice
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1239, 714)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.DataGridView)
+        Me.Controls.Add(Me.btnNext)
+        Me.Controls.Add(Me.btnFind)
+        Me.Controls.Add(Me.btnUpdate)
+        Me.Controls.Add(Me.ComboBoxCopyTo)
+        Me.Controls.Add(Me.TextBoxDocStatus)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.TextBoxDocTotal)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.TextBoxDocNumber)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.btnPrevious)
+        Me.Controls.Add(Me.btnAdd)
+        Me.Controls.Add(Me.btnCancel)
+        Me.Controls.Add(Me.BtnOK)
+        Me.Controls.Add(Me.DateTimePickerTaxDate)
+        Me.Controls.Add(Me.DateTimePickerDocDueDate)
+        Me.Controls.Add(Me.DateTimePickerDocDate)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.TextBoxBPName)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.TextBoxBPCode)
+        Me.Controls.Add(Me.btnChooseCustomer)
+        Me.Controls.Add(Me.Label2)
+        Me.Name = "ARInvoice"
+        Me.Text = "ARInvoice"
+        CType(Me.DataGridView, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents ItemCode As System.Windows.Forms.DataGridViewButtonColumn
+    Friend WithEvents Quantity As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents Label10 As System.Windows.Forms.Label
+    Friend WithEvents ItemName As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents UnitPrice As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents DataGridView As System.Windows.Forms.DataGridView
+    Friend WithEvents Total As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents btnNext As System.Windows.Forms.Button
+    Friend WithEvents btnFind As System.Windows.Forms.Button
+    Friend WithEvents btnUpdate As System.Windows.Forms.Button
+    Friend WithEvents ComboBoxCopyTo As System.Windows.Forms.ComboBox
+    Friend WithEvents TextBoxDocStatus As System.Windows.Forms.TextBox
+    Friend WithEvents Label9 As System.Windows.Forms.Label
+    Friend WithEvents TextBoxDocTotal As System.Windows.Forms.TextBox
+    Friend WithEvents Label8 As System.Windows.Forms.Label
+    Friend WithEvents TextBoxDocNumber As System.Windows.Forms.TextBox
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents btnPrevious As System.Windows.Forms.Button
+    Friend WithEvents btnAdd As System.Windows.Forms.Button
+    Friend WithEvents btnCancel As System.Windows.Forms.Button
+    Friend WithEvents BtnOK As System.Windows.Forms.Button
+    Friend WithEvents DateTimePickerTaxDate As System.Windows.Forms.DateTimePicker
+    Friend WithEvents DateTimePickerDocDueDate As System.Windows.Forms.DateTimePicker
+    Friend WithEvents DateTimePickerDocDate As System.Windows.Forms.DateTimePicker
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents TextBoxBPName As System.Windows.Forms.TextBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents TextBoxBPCode As System.Windows.Forms.TextBox
+    Friend WithEvents btnChooseCustomer As System.Windows.Forms.Button
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+End Class
